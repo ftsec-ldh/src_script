1017a72c0ac03491104eed20adea73ded28518aa
fun <128038117+1607131160@users.noreply.github.com>
2024-03-16 14:13:02 +0800

    Update private_script.py

--- private_script.py
+++ private_script.py
@@ -20,15 +20,17 @@ if __name__ == "__main__":
         if choice == "1":
             file_name = input("请输入文件名：")
             with open(file_name,"r+") as input:
-                for i in input:
-                    i = i.strip()
-                    url = i
-                    if re.search(r"\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}",i):#检测到IP自动反查域名
-                        domain = get_domain_byIP(i)
-                        if domain:
-                            print(f"ip：{url}，域名：{domain},公司名：{get_company(domain)}, 权重：{get_rank(domain)}", end="\n")
-                            continue
-                        else:
-                            print(i.replace("http://","").replace("https://","") + "未绑定域名，跳过此次查询",end="\n")
-                            continue
-                    print(f"站点：{url},公司名：{get_company(i)}, 权重：{get_rank(i)}",end="\n")
+                lines = input.readlines()
+
+            for line in lines:
+                line = line.strip()
+                url = line
+                if re.search(r"\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}",i):#检测到IP自动反查域名
+                    domain = get_domain_byIP(line)
+                    if domain:
+                        print(f"ip：{url}，域名：{domain},公司名：{get_company(domain)}, 权重：{get_rank(domain)}", end="\n")
+                        continue
+                    else:
+                        print(line.replace("http://","").replace("https://","") + "未绑定域名，跳过此次查询",end="\n")
+                        continue
+                print(f"站点：{url},公司名：{get_company(line)}, 权重：{get_rank(line)}",end="\n")
