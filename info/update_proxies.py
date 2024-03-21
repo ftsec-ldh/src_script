c1ab04f95c73388dd6c1fd4e3c8844951618011b
fun <128038117+1607131160@users.noreply.github.com>
2024-03-21 15:18:16 +0800

    Update update_proxies.py

--- info/update_proxies.py
+++ info/update_proxies.py
@@ -1,12 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+import threading
+import time
 
 with open("conf/proxies.conf") as input:
     proxy_server = input.read().strip()#proxypool服务器
 
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.5359.72 Safari/537.36'}
 
-def check(https_proxy,proxies):
+def check(https_proxy):
     try:
         res = requests.get("http://site.ip138.com/106.75.184.166", headers=headers, timeout=5,proxies={"http": f"http://{https_proxy}"}, verify=False)
         text = res.text
@@ -17,22 +19,31 @@ def check(https_proxy,proxies):
     except Exception:
         return False
 
+def thread_check(dict,proxies):
+    https_proxy = dict['proxy']
+    if check(https_proxy):
+        proxies.append(https_proxy + "\n")
+        print(f"{https_proxy}存活性良好")
+    else:
+        print(f"代理池{https_proxy}出错")
+        requests.get(f"http://{proxy_server}/delete?proxy={https_proxy}")  # 删除失效代理
+
+
 def update_proxy():
-    alive = 0
-    proxies = list()
+    proxies = []
+    threads = []
     all_https_proxy = requests.get(f"http://{proxy_server}/all?type=http").json()
     print(f"检测到共存在{len(all_https_proxy)}个代理池")
     for dict in all_https_proxy:
-        https_proxy = dict['proxy']
-        if check(https_proxy,proxies):
-            proxies.append(https_proxy + "\n")
-            alive += 1
-            print(f"{https_proxy}存活性良好")
-        else:
-            print(f"代理池{https_proxy}出错")
-            requests.get(f"http://{proxy_server}/delete?proxy={https_proxy}")#删除失效代理
+        thread = threading.Thread(target=thread_check,args=[dict,proxies])
+        threads.append(thread)
+        thread.start()
+        time.sleep(0.5)
+
+    for thread in threads:
+        thread.join()
 
     with open("proxies.txt", "w+") as output:
         output.writelines(proxies)
 
-    print(f"更新代理池完毕，存活{alive}个，已自动删除无效代理")
+    print(f"更新代理池完毕，存活{len(proxies)}个，已自动删除无效代理")
