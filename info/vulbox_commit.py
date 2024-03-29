6ab1b4753e77ecaf9bebff5a11537bc01f45532f
fun <128038117+1607131160@users.noreply.github.com>
2024-03-29 19:08:08 +0800

    Update vulbox_commit.py

--- info/vulbox_commit.py
+++ info/vulbox_commit.py
@@ -11,7 +11,8 @@ import time,os
 description = {"信息泄露":"信息泄露可能会导致黑客进一步利用敏感信息盗取更关键的内容，甚至导致系统产生RCE漏洞",
                "CSRF":"csrf会造成第三者的滥用",
                "SQL注入":"攻击者未经授权可以访问数据库中的数据",
-               "XSS":"反射xss通过引诱用户点击一个链接到目标网站的恶意链接来实施攻击,dom型则更具危害性，受害者正常进入页面则会遭受攻击"
+               "XSS":"反射xss通过引诱用户点击一个链接到目标网站的恶意链接来实施攻击,dom型则更具危害性，受害者正常进入页面则会遭受攻击",
+               "弱口令":"弱口令可能导致未经授权的用户获得对系统、应用程序或数据的访问权限。这可能会导致数据泄露、篡改、删除或者系统被滥用,如果用户在多个网站或服务中使用相同的弱密码，一旦其中一个网站的密码泄露，攻击者就能够使用该密码尝试登录其他网站，进而导致更大范围的信息泄露"
                }
 
 suggestions = {
@@ -19,10 +20,10 @@ suggestions = {
     "SQL注入":"严格限制变量类型，比如整型变量就采用intval()函数过滤，数据库中的存储字段必须对应为int型",
     "XSS":"白名单过滤 根据白名单的标签和属性对数据进行过滤，以此来对可执行的脚本进行清除(如script标签，img标签的onerror属性等",
     "信息泄露":"设置访问该信息的权限配置，或只允许某一特定IP访问",
+    "弱口令":"使用足够长度、包含大小写字母、数字和特殊字符的密码；还可以实施密码策略，要求用户设置符合安全标准的密码，如长度、复杂度要求，并对密码进行定期的过期和强制更改"
 }
 
-def vulbox_login(user,passwd,domain,leak_type,company_name):#检测到没有cookie再执行这一步拿cookie
-    title = company_name + "页面存在" + leak_type
+def vulbox_login(user,passwd):#检测到没有cookie再执行这一步拿cookie
     s = Service("drivers/win64/chromedriver.exe")
     driver_vulbox = webdriver.Chrome(service=s)
     driver_vulbox.get("https://www.vulbox.com/account/login")
@@ -39,32 +40,16 @@ def vulbox_login(user,passwd,domain,leak_type,company_name):#检测到没有cook
     cookies = driver_vulbox.get_cookies()
     with open('vulbox_cookies.txt','w+') as output:
         output.write(str(cookies))
-    driver_vulbox.get("https://user.vulbox.com/management/submit/72")
-    driver_vulbox.find_element(By.ID, "register_bug_title").send_keys(title)  # 漏洞标题
-    time.sleep(15)
-
-    driver_vulbox.find_element(By.XPATH, "//input[@type='radio' and @class='ant-radio-input' and @value='1']").click()#漏洞类别勾选
-    driver_vulbox.find_element(By.XPATH, "//input[@type='radio' and @class='ant-radio-input' and @value='0']").click()  # 参与评定勾选
-    driver_vulbox.find_element(By.ID, "register_domain").send_keys(domain)  # 厂商域名
-    driver_vulbox.find_elements(By.XPATH, "//input[@type='radio' and @value='2']")[1].click()#漏洞等级
-    elements = driver_vulbox.find_elements(By.XPATH, '//p[@data-we-empty-p]')  # 修复建议
-    driver_vulbox.execute_script("arguments[0].innerText = arguments[1];", elements[1], suggestions[leak_type])
-
-    elements = driver_vulbox.find_elements(By.TAG_NAME, "textarea")
-    driver_vulbox.execute_script("arguments[0].value = arguments[1];", elements[0], description[leak_type])#漏洞简述
-    time.sleep(2)
-    driver_vulbox.execute_script("arguments[0].value = arguments[1];", elements[0],
-                                 description[leak_type])  # 不知道为什么要执行两遍才可以...
-
-
-    input()
+    driver_vulbox.quit()
+    print("请重新执行该软件")
 
 def vulbox_src_page(domain,leak_type,leak_url):
-    company_name = get_company(domain)
-    area_dict = aiqicha_get(company_name)
-    title = company_name + "页面存在" + leak_type
-
     if os.path.exists("vulbox_cookies.txt"):
+
+        company_name = get_company(domain)#获取公司名
+        area_dict = aiqicha_get(company_name)#获取公司地址、注册资金、行业划分
+        title = company_name + "页面存在" + leak_type
+
         s = Service("drivers/win64/chromedriver.exe")
         driver_vulbox = webdriver.Chrome(service=s)
         driver_vulbox.get("https://www.vulbox.com/account/login")
@@ -78,9 +63,7 @@ def vulbox_src_page(domain,leak_type,leak_url):
         actions = ActionChains(driver_vulbox)  # 实例化actions类
 
         #######厂商名称##########
-        WebDriverWait(driver_vulbox, 10).until(
-            EC.presence_of_element_located((By.XPATH, f"//div[@class='ant-select-selection__rendered']"))
-        )#等待元素
+        time.sleep(3)
 
         element_leak_company = driver_vulbox.find_elements(By.XPATH,f"//div[@class='ant-select-selection__rendered']")[2]
         actions.send_keys_to_element(element_leak_company, company_name).pause(2).perform()
@@ -103,13 +86,10 @@ def vulbox_src_page(domain,leak_type,leak_url):
             EC.presence_of_element_located((By.XPATH, f"//li[@title='{leak_type}']"))
         )#等待元素
 
-        if leak_type == "信息泄露":
-            element_choice = driver_vulbox.find_element(By.XPATH, "//li[@title='信息泄露']")
-            actions.click(element_choice).perform()
+        element_choice = driver_vulbox.find_element(By.XPATH, f"//li[@title='{leak_type}']")
+        actions.click(element_choice).perform()
 
-        if leak_type == "XSS":
-            element_to_click = driver_vulbox.find_element(By.XPATH, f"//li[@title='反射型XSS']")
-            actions.click(element_to_click).perform()
+
         #######漏洞类型##########
 
         leak_title = driver_vulbox.find_element(By.ID, "register_bug_title")# 漏洞标题
@@ -121,12 +101,11 @@ def vulbox_src_page(domain,leak_type,leak_url):
         choice = driver_vulbox.find_element(By.XPATH, "//input[@type='radio' and @value='0']")#参与评定勾选
         actions.click(choice)
 
-        company_domain = driver_vulbox.find_element(By.ID, "register_domain")#厂商域名
-        actions.send_keys_to_element(company_domain,domain).perform()
-
-        choice = driver_vulbox.find_elements(By.XPATH, "//input[@type='radio' and @value='2']")[1]#漏洞等级
+        choice = driver_vulbox.find_elements(By.XPATH, "//input[@type='radio' and @value='2']")[1]  # 漏洞等级-中危
         actions.click(choice).perform()
 
+        company_domain = driver_vulbox.find_element(By.ID, "register_domain")#厂商域名
+        actions.send_keys_to_element(company_domain,domain).perform()
 
         elements = driver_vulbox.find_elements(By.XPATH, "//p[@data-we-empty-p]")[1]#修复建议
         actions.send_keys_to_element(elements,suggestions[leak_type]).perform()
@@ -139,7 +118,7 @@ def vulbox_src_page(domain,leak_type,leak_url):
 
         province = area_dict["province"]
 
-        element = driver_vulbox.find_elements(By.XPATH,"//span[@class='ant-cascader-picker-label']")[1]
+        element = driver_vulbox.find_element(By.XPATH,"//input[@placeholder='请选择所属地区']")
         actions.click(element).perform()
 
         WebDriverWait(driver_vulbox, 10).until(
@@ -154,7 +133,7 @@ def vulbox_src_page(domain,leak_type,leak_url):
             city = province + "市"
 
             WebDriverWait(driver_vulbox, 10).until(
-                EC.presence_of_element_located((By.XPATH, f"//li[@title='{city}'"))
+                EC.presence_of_element_located((By.XPATH, f"//li[@title='{city}']"))
             )  # 等待元素
 
             element = driver_vulbox.find_element(By.XPATH,f"//li[@title='{city}']")
@@ -177,6 +156,8 @@ def vulbox_src_page(domain,leak_type,leak_url):
         input()
 
     else:
-        print("未检测到cookie文件，即将开始登录")
-        vulbox_login("username","password",domain,leak_type,company_name)#输入你的账号和密码
+        print("未检测到cookie文件，请手动登录")
+        username = input("请输入你的账号：")
+        password = input("请输入你的密码：")
+        vulbox_login(username,password)
 
