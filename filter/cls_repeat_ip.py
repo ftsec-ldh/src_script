9bd9d0739cf852dbb326b8a78cf472bb9371bf82
fun <128038117+1607131160@users.noreply.github.com>
2024-06-14 13:29:13 +0800

    Update cls_repeat_ip.py

--- filter/cls_repeat_ip.py
+++ filter/cls_repeat_ip.py
@@ -1,1 +1,12 @@
+def remove_duplicates(filename):
+    lines_seen = set()
+    output_filename = filename.split('.')[0] + "_nodupes.txt"
 
+    with open(filename, 'r') as input_file, open(output_filename, 'w') as output_file:
+        for line in input_file:
+            line = line.strip()  # 去除行尾的换行符和空格
+            if line not in lines_seen:
+                output_file.write(line + '\n')
+                lines_seen.add(line)
+
+    print("去除重复行完成！保存为：", output_filename)
