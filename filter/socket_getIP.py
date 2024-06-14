bd44a8d167acbbefb6aab416610c8e352579780c
fun <128038117+1607131160@users.noreply.github.com>
2024-06-14 13:29:45 +0800

    Update socket_getIP.py

--- filter/socket_getIP.py
+++ filter/socket_getIP.py
@@ -1,1 +1,9 @@
 
+import socket
+def domain_to_ip(domain):
+    try:
+        ip = socket.gethostbyname(domain.strip())
+        return ip
+    except socket.error as e:
+        print(f"Error converting {domain} to IP: {e}")
+        return None
