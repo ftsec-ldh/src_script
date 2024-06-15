0d9d538b3621d2a1a278adcff320ebdce36e7ea7
fun <128038117+1607131160@users.noreply.github.com>
2024-06-15 15:12:17 +0800

    Update dirsearch.py

--- scan/dirsearch.py
+++ scan/dirsearch.py
@@ -1,6 +1,12 @@
 import os
 
-def dirscan(dirsearch_path, url):
-    command = f'python "{dirsearch_path}dirsearch.py" -u "{url}"'
-    os.system(f'start cmd.exe /K {command}')
+def dirscan(dirsearch_path, url,thread,language,proxy):
+    if len(proxy) == 0:
+        command = f'python "{dirsearch_path}dirsearch.py" -u "{url}" -t {thread} -e {language}'
+        os.system(f'start cmd.exe /K {command}')
 
+    if len(proxy) != 0:
+        command = f'python "{dirsearch_path}dirsearch.py" -u "{url}" -t {thread} -e {language} --proxy {proxy}'
+        os.system(f'start cmd.exe /K {command}')
+
+#扫描完毕提示Task Completed
