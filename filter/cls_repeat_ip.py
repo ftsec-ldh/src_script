3947c1ea2244a656aeb1f7d0fa8c645be17d9efc
fun <128038117+1607131160@users.noreply.github.com>
2024-06-25 10:34:51 +0800

    Update cls_repeat_ip.py

--- filter/cls_repeat_ip.py
+++ filter/cls_repeat_ip.py
@@ -16,29 +16,30 @@ def remove_duplicates(filename):
     print("去除重复行完成！保存为：", output_filename)
 
 def remove_same_ip(filename):
-    output_filename = filename.split('.')[0] + "_无重ip.txt"
-
     with open(filename, 'r') as input_file:
-        unique_ip_targets = []
-        unique_targets = []
 
+        target_number = len(input_file.readlines())
+        input_file.seek(0)#指针重新回到0
+
+        unique_ip_targets = []
+        进度 = 0
         for line in input_file:
             line = line.strip()#去除行尾的换行符和空格
             main = get_main(line)
 
             ip_pattern = r"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b"
             ip_match = re.search(ip_pattern, main)
-            if ip_match:#说明是ip
-                if main not in unique_ip_targets:
-                    unique_ip_targets.append(line)
-            else:#说明是域名
-                ip = domain_to_ip(main)
-                if ip not in unique_ip_targets:
-                    unique_targets.append(line)
-                unique_ip_targets.append(ip)
-
-
-    with open("无重ip.txt","a+") as input_file:
-        for i in unique_targets:
-            i = i.strip()
-            input_file.write(i + "\n")
+            with open("无重ip.txt", "a+") as input_file:
+                if ip_match:#说明是ip
+                    if main not in unique_ip_targets:
+                        input_file.write(line + "\n")
+                    unique_ip_targets.append(main)
+                        #unique_ip_targets.append(line)
+                else:#说明是域名
+                    ip = domain_to_ip(main)
+                    if ip not in unique_ip_targets:
+                        input_file.write(line + "\n")
+                        #unique_targets.append(line)
+                    unique_ip_targets.append(ip)
+                print(f"进度：{进度}/{target_number}")
+                进度 += 1
