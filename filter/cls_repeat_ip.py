8c87bf2b6ef047a6b26ca3db3145a7b0f6040e2c
fun <128038117+1607131160@users.noreply.github.com>
2024-06-26 12:01:23 +0800

    Update cls_repeat_ip.py

--- filter/cls_repeat_ip.py
+++ filter/cls_repeat_ip.py
@@ -1,6 +1,6 @@
 from info.api import get_main
 from filter.socket_getIP import domain_to_ip
-import re
+import re,threading
 
 def remove_duplicates(filename):
     lines_seen = set()
@@ -15,31 +15,48 @@ def remove_duplicates(filename):
 
     print("去除重复行完成！保存为：", output_filename)
 
-def remove_same_ip(filename):
-    with open(filename, 'r') as input_file:
+def thread_domain_to_ip(main,unique_ip_targets,line,进度,目标和):
+    with open("无重ip.txt", "a+") as input_file:
+        ip = domain_to_ip(main)
+        if ip not in unique_ip_targets:
+            input_file.write(line + "\n")
+        unique_ip_targets.append(ip)
 
-        target_number = len(input_file.readlines())
-        input_file.seek(0)#指针重新回到0
+def remove_same_ip(filename,thread=0):
+    if thread == 1:
+        with open(filename, 'r') as input_file:
+            进度 = 0
+            目标和 = len(input_file.readlines())
+            input_file.seek(0)#指针重新回到0
+            unique_ip_targets = []
+            threads = []
+            for line in input_file:
+                line = line.strip()#去除行尾的换行符和空格
+                main = get_main(line)
 
-        unique_ip_targets = []
-        进度 = 0
-        for line in input_file:
-            line = line.strip()#去除行尾的换行符和空格
-            main = get_main(line)
+                thread = threading.Thread(target=thread_domain_to_ip,args=[main,unique_ip_targets,line,进度,目标和])
+                threads.append(thread)
 
-            ip_pattern = r"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b"
-            ip_match = re.search(ip_pattern, main)
-            with open("无重ip.txt", "a+") as input_file:
-                if ip_match:#说明是ip
-                    if main not in unique_ip_targets:
-                        input_file.write(line + "\n")
-                    unique_ip_targets.append(main)
-                        #unique_ip_targets.append(line)
-                else:#说明是域名
+            for i in threads:
+                i.start()
+                进度 += 1
+                print(f"进度：{进度}/{目标和}")
+    if thread == 0:
+        with open(filename, 'r') as input_file:
+            target_number = len(input_file.readlines())
+            input_file.seek(0)#指针重新回到0
+            unique_ip_targets = []
+            进度 = 0
+            for line in input_file:
+                line = line.strip()#去除行尾的换行符和空格
+                main = get_main(line)
+
+                ip_pattern = r"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b"
+                ip_match = re.search(ip_pattern, main)
+                with open("无重ip.txt", "a+") as input_file:
                     ip = domain_to_ip(main)
                     if ip not in unique_ip_targets:
                         input_file.write(line + "\n")
-                        #unique_targets.append(line)
                     unique_ip_targets.append(ip)
                 print(f"进度：{进度}/{target_number}")
                 进度 += 1
