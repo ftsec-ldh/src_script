0a92b77ede0fc9e37879b0ecc6d92ed1e0ff841c
fun <128038117+1607131160@users.noreply.github.com>
2024-08-27 03:22:34 +0800

    Update oneforall.py

--- scan/oneforall.py
+++ scan/oneforall.py
@@ -1,9 +1,12 @@
-import os,ast
+import os
+import ast
 import pandas as pd
-import glob,subprocess
+import glob
+import subprocess
 import numpy as np
 
-def domain_scan(domain_search):#oneforall联动subfinder
+
+def domain_scan(domain_search):# oneforall联动subfinder
     with open("conf/config.conf", encoding="utf-8") as config_file:
         config = config_file.read()
         config = ast.literal_eval(config)
@@ -13,27 +16,33 @@ def domain_scan(domain_search):#oneforall联动subfinder
 
     urls = set()
 
-    command = ['python', f'{oneforall_path}\oneforall.py', '--valid=None', '--verify=False', f'--target={domain_search}', 'run']
+    command = ['python', f'{oneforall_path}/oneforall.py', '--valid=None', '--verify=False',f'--target={domain_search}', 'run']
     print(f"正在调用OneForAll收集域名：{domain_search}，请耐心等待！")
-    subprocess.run(command, capture_output=True, text=True)
     try:
-        df = pd.read_csv(f"{oneforall_path}\\results\\{domain_search}.csv", encoding='utf-8')
+        subprocess.run(command, capture_output=True, text=True,encoding='utf-8')
+    except Exception as e:
+        print(f"输出：{e}")
+
+    try:
+        df = pd.read_csv(f"{oneforall_path}/results/{domain_search}.csv", encoding='utf-8')
     except UnicodeDecodeError:
-        df = pd.read_csv(f"{oneforall_path}\\results\\{domain_search}.csv", encoding='latin1')
+        df = pd.read_csv(f"{oneforall_path}/results/{domain_search}.csv", encoding='latin1')
 
     valid_urls = df['subdomain'].tolist()
     for url in valid_urls:
         urls.add(url)
 
-    command = [f'{subfinder_path}\\subfinder.exe', '-d', domain_search]
+    command = [f'{subfinder_path}/subfinder.exe', '-d', domain_search]
     print(f"正在调用SubFinder收集域名：{domain_search}，请耐心等待！")
-    result = subprocess.run(command, capture_output=True, text=True)
-    for url in result.stdout.split("\n"):
-        urls.add(url)
+    result = subprocess.run(command, capture_output=True, text=True,encoding='utf-8')
 
-    while np.nan in urls:
-        urls.remove(np.nan)#删除nan类型的float元素，防止报错
-    urls.remove('')
+    if result.stdout:#检查输出是否存在
+        for url in result.stdout.split("\n"):
+            if url:#确保url不为空
+                urls.add(url)
+
+    # 删除nan类型的float元素
+    urls.discard(np.nan)
+    urls.discard('')
 
     return urls
-
