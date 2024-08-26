20c4922d41b6de3c8bb5d2a2c9865b751c61720d
fun <128038117+1607131160@users.noreply.github.com>
2024-08-27 02:36:27 +0800

    Update oneforall.py

--- scan/oneforall.py
+++ scan/oneforall.py
@@ -1,26 +1,7 @@
-import os
+import os,ast
 import pandas as pd
-import glob
+import glob,subprocess
 from urllib.parse import urlparse
-
-# 读取文件中的URLs
-def read_urls_from_file(filename):
-    with open(filename, 'r') as file:
-        return file.readlines()
-
-# 提取并排重域名，优先保留HTTPS
-def extract_unique_domains(urls):
-    domain_dict = {}
-    for url in urls:
-        parsed_url = urlparse(url.strip())
-        scheme = parsed_url.scheme
-        netloc = parsed_url.netloc
-
-        # 如果当前域名不存在，或者存在但当前是HTTPS则更新/添加
-        if netloc not in domain_dict or scheme == 'https':
-            domain_dict[netloc] = url.strip()
-
-    return domain_dict.values()
 
 # 保存排重后的域名到文件
 def save_domains_to_file(domains, filename):
@@ -29,15 +10,56 @@ def save_domains_to_file(domains, filename):
             file.write(domain + '\n')
 
 
-def domain_scan(oneforall_path,url):
-    command = f'python "{oneforall_path}oneforall.py" --target {url} run'
+def domain_scan(domain_search):#oneforall联动subfinder
+    with open("conf/config.conf", encoding="utf-8") as config_file:
+        config = config_file.read()
+        config = ast.literal_eval(config)
+
+    oneforall_path = config['oneforall_path']
+    subfinder_path = config['subfinder_path']
+
+    urls = set()
+
+    command = ['python', f'{oneforall_path}\oneforall.py', '--valid=None', '--verify=False', f'--target={url}', 'run']
+    print(f"即将调用OneForAll收集域名：{url}，请耐心等待！")
+    subprocess.run(command, capture_output=True, text=True)
+    try:
+        df = pd.read_csv(f"{oneforall_path}\\results\\{domain_search}.csv", encoding='utf-8')
+    except UnicodeDecodeError:
+        df = pd.read_csv(f"{oneforall_path}\\results\\{domain_search}.csv", encoding='latin1')
+
+    valid_urls = df['subdomain'].tolist()
+    for url in valid_urls:
+        urls.add(url)
+
+
+    command = [f'{subfinder_path}\\subfinder.exe', '-d', domain_search]
+    print(f"即将调用SubFinder收集域名：{domain_search}，请耐心等待！")
+    result = subprocess.run(command, capture_output=True, text=True)
+    for url in result.stdout.split("\n"):
+        urls.add(url)
+    urls.remove('')
+    return urls
+
+
+
+def domains_scan(file_name):
+    with open("conf/config.conf", encoding="utf-8") as config_file:
+        config = config_file.read()
+        config = ast.literal_eval(config)
+    oneforall_path = config['oneforall_path']
+
+    command = f'python "{oneforall_path}oneforall.py" --valid=None --verify=False --targets {file_name} run'
     os.system(f'start cmd.exe /K {command}')
 
-def domains_scan(oneforall_path,file_name):
-    command = f'python "{oneforall_path}oneforall.py" --targets {file_name} run'
-    os.system(f'start cmd.exe /K {command}')
 
-def filter_validIP(oneforall_path,domain_name):
+def filter_validIP(domain_name):
+
+    with open("conf/config.conf", encoding="utf-8") as config_file:
+        config = config_file.read()
+        config = ast.literal_eval(config)
+    oneforall_path = config['oneforall_path']
+
     urls = []
     try:
         df = pd.read_csv(f"{oneforall_path}results\\{domain_name}.csv", encoding='utf-8')
@@ -54,7 +76,12 @@ def filter_validIP(oneforall_path,domain_name):
     print("提取完毕!!!!!!!!!!!!!")
     print("-------------提取信息-------------")
 
-def filter_validIPs(oneforall_path):
+def filter_validIPs():
+    with open("conf/config.conf", encoding="utf-8") as config_file:
+        config = config_file.read()
+        config = ast.literal_eval(config)
+    oneforall_path = config['oneforall_path']
+
     csv_files = glob.glob(f'{oneforall_path}results\*.csv')
     urls = []
     for file in csv_files:
