324a5837b7925f83334f2f2d194b2765ddb782ca
fun <128038117+1607131160@users.noreply.github.com>
2024-08-27 19:47:00 +0800

    Update oneforall.py

--- scan/oneforall.py
+++ scan/oneforall.py
@@ -8,43 +8,43 @@ import numpy as np
 
 
 def domain_scan(domain_search):# oneforall联动subfinder
-    # with open("conf/config.conf", encoding="utf-8") as config_file:
-    #     config = config_file.read()
-    #     config = ast.literal_eval(config)
-    #
-    # oneforall_path = config['oneforall_path']
-    # subfinder_path = config['subfinder_path']
-    #
+    with open("conf/config.conf", encoding="utf-8") as config_file:
+        config = config_file.read()
+        config = ast.literal_eval(config)
+
+    oneforall_path = config['oneforall_path']
+    subfinder_path = config['subfinder_path']
+
     urls = set()
-    #
-    # command = ['python', f'{oneforall_path}/oneforall.py', '--valid=None', '--verify=False',f'--target={domain_search}', 'run']
-    # print(f"正在调用OneForAll收集域名：{domain_search}，请耐心等待！")
-    # try:
-    #     subprocess.run(command, capture_output=True, text=True,encoding='utf-8')
-    # except Exception as e:
-    #     print(f"输出：{e}")
-    #
-    # try:
-    #     df = pd.read_csv(f"{oneforall_path}/results/{domain_search}.csv", encoding='utf-8')
-    # except UnicodeDecodeError:
-    #     df = pd.read_csv(f"{oneforall_path}/results/{domain_search}.csv", encoding='latin1')
-    #
-    # valid_urls = df['subdomain'].tolist()
-    # for url in valid_urls:
-    #     urls.add(url)
-    #
-    # command = [f'{subfinder_path}/subfinder.exe', '-d', domain_search]
-    # print(f"正在调用SubFinder收集域名：{domain_search}，请耐心等待！")
-    # result = subprocess.run(command, capture_output=True, text=True,encoding='utf-8')
-    #
-    # if result.stdout:#检查输出是否存在
-    #     for url in result.stdout.split("\n"):
-    #         if url:#确保url不为空
-    #             urls.add(url)
-    #
-    # # 删除nan类型的float元素
-    # urls.discard(np.nan)
-    # urls.discard('')
+
+    command = ['python', f'{oneforall_path}/oneforall.py', '--valid=None', '--verify=False',f'--target={domain_search}', 'run']
+    print(f"正在调用OneForAll收集域名：{domain_search}，请耐心等待！")
+    try:
+        subprocess.run(command, capture_output=True, text=True,encoding='utf-8')
+    except Exception as e:
+        print(f"输出：{e}")
+
+    try:
+        df = pd.read_csv(f"{oneforall_path}/results/{domain_search}.csv", encoding='utf-8')
+    except UnicodeDecodeError:
+        df = pd.read_csv(f"{oneforall_path}/results/{domain_search}.csv", encoding='latin1')
+
+    valid_urls = df['subdomain'].tolist()
+    for url in valid_urls:
+        urls.add(url)
+
+    command = [f'{subfinder_path}/subfinder.exe', '-d', domain_search]
+    print(f"正在调用SubFinder收集域名：{domain_search}，请耐心等待！")
+    result = subprocess.run(command, capture_output=True, text=True,encoding='utf-8')
+
+    if result.stdout:#检查输出是否存在
+        for url in result.stdout.split("\n"):
+            if url:#确保url不为空
+                urls.add(url)
+
+    # 删除nan类型的float元素
+    urls.discard(np.nan)
+    urls.discard('')
     from selenium import webdriver
     from selenium.webdriver.chrome.service import Service
     from selenium.webdriver.common.by import By
@@ -104,8 +104,6 @@ def domain_scan(domain_search):# oneforall联动subfinder
     for tag_a_element in tag_a_elements:
         if domain_search in tag_a_element.text:
             urls.add(tag_a_element.text)
-            print(tag_a_element.text)
 
-    input()
     return urls
 
