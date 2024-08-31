46e854f0cfed1622367afd53dc7f9b7599ab59ab
fun <128038117+1607131160@users.noreply.github.com>
2024-08-31 18:39:48 +0800

    Update cnvd_commit.py

--- info/cnvd_commit.py
+++ info/cnvd_commit.py
@@ -3,7 +3,9 @@ from info.api import get_company,aiqicha_get
 from filter.socket_getIP import domain_to_ip
 from selenium.webdriver.support.ui import Select
 from docx import Document
-from docx.shared import Inches
+from docx.shared import Inches,Pt
+from docx.oxml.ns import qn
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 
 def cnvd_src_page(domain, leak_type, leak_url):
 
@@ -14,15 +16,15 @@ def cnvd_src_page(domain, leak_type, leak_url):
                    "弱口令": "弱口令可能导致未经授权的用户获得对系统、应用程序或数据的访问权限。这可能会导致数据泄露、篡改、删除或者系统被滥用,如果用户在多个网站或服务中使用相同的弱密码，一旦其中一个网站的密码泄露，攻击者就能够使用该密码尝试登录其他网站，进而导致更大范围的信息泄露",
                    "命令执行": "远程代码执行简称RCE，是一类软件安全缺陷/漏洞。RCE 漏洞将允许恶意行为人通过 LAN、WAN 或 Internet 在远程计算机上执行自己选择的任何代码，属于更广泛的任意代码执行 (ACE) 漏洞类别。"
 
-                   }
+    }
 
     suggestions = {
-        "逻辑漏洞": "修复不严谨的业务判断逻辑，对关键参数和用户COOKIE或鉴权认证进行校验",
+        "逻辑缺陷": "修复不严谨的业务判断逻辑，对关键参数和用户COOKIE或鉴权认证进行校验",
         "SQL注入": "严格限制变量类型，比如整型变量就采用intval()函数过滤，数据库中的存储字段必须对应为int型",
         "XSS": "白名单过滤 根据白名单的标签和属性对数据进行过滤，以此来对可执行的脚本进行清除(如script标签，img标签的onerror属性等",
         "信息泄露": "设置访问该信息的权限配置，或只允许某一特定IP访问",
         "弱口令": "使用足够长度、包含大小写字母、数字和特殊字符的密码；还可以实施密码策略，要求用户设置符合安全标准的密码，如长度、复杂度要求，并对密码进行定期的过期和强制更改",
-        "代码执行": "1.通用的修复方案，升级插件/框架/服务最新版\n2.如若必须使用危险函数，那么针对危险函数进行过滤\n3.在进入执行命令函数前进行严格的检测和过滤\n4.尽量不要使用命令执行函数，不能完全控制的危险函数最好不使用，如果非要用的话可以加验证防止被其他人利用"
+        "命令执行": "1.通用的修复方案，升级插件/框架/服务最新版\n2.如若必须使用危险函数，那么针对危险函数进行过滤\n3.在进入执行命令函数前进行严格的检测和过滤\n4.尽量不要使用命令执行函数，不能完全控制的危险函数最好不使用，如果非要用的话可以加验证防止被其他人利用"
     }
 
 
@@ -32,11 +34,36 @@ def cnvd_src_page(domain, leak_type, leak_url):
             weak_account = input("请输入弱口令账号：")
             weak_password = input("请输入弱口令密码：")
 
-        company_name = get_company(domain)
+        print("正在使用爱站查询域名信息，请耐心等待...")
+        company_name = get_company(domain,1)
+
+        print("正在使用爱企查查询企业信息，请耐心等待...")
+        company_info = aiqicha_get(company_name, 1)
+
         ip = domain_to_ip(domain)
-        company_info = aiqicha_get(company_name,1)
         province = company_info['province']
         city = company_info['city']
+
+        if leak_type == "弱口令":
+            doc = Document()
+            paragraph = doc.add_paragraph()
+            run = paragraph.add_run(f'{company_name}网站存在{leak_type}\n\n弱口令账号：{weak_account}\n弱口令密码：{weak_password}')
+            run.font.name = '等线'
+            run.font.size = Pt(12)
+            run._element.rPr.rFonts.set(qn('w:eastAsia'), '等线')
+            doc.add_picture(f"aizhan_{company_name}.png", width=Inches(8.0))
+            doc.add_picture(f"aiqicha_{company_name}.png", width=Inches(8.0))
+            doc.save(f'{company_name}存在{leak_type}.docx')
+        else:
+            doc = Document()
+            paragraph = doc.add_paragraph()
+            run = paragraph.add_run(f'{company_name}网站存在{leak_type}')
+            run.font.name = '等线'
+            run.font.size = Pt(12)
+            run._element.rPr.rFonts.set(qn('w:eastAsia'), '等线')
+            doc.add_picture(f"aizhan_{company_name}.png", width=Inches(8.0))
+            doc.add_picture(f"aiqicha_{company_name}.png", width=Inches(8.0))
+            doc.save(f'{company_name}存在{leak_type}.docx')
 
         from selenium import webdriver
         from selenium.webdriver.chrome.service import Service
@@ -125,6 +152,11 @@ def cnvd_src_page(domain, leak_type, leak_url):
 
         element = WebDriverWait(cnvd_driver, 10).until(EC.presence_of_element_located((By.XPATH, f"//textarea[@name='tempWay']")))#临时解决方案
         element.send_keys(f"{suggestions[leak_type]}")
+
+        element = WebDriverWait(cnvd_driver, 10).until(EC.presence_of_element_located((By.XPATH, f"//input[@id='flawAttFile']")))#文件上传
+        print(f"{os.getcwd()}/{company_name}存在{leak_type}.docx")
+        element.send_keys(f"{os.getcwd()}\{company_name}存在{leak_type}.docx")
+
 
         #用户中心
         input()
