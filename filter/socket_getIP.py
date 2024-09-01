f4320125f6c4d9bb5fd57f3a7e21026668e478a1
fun <128038117+1607131160@users.noreply.github.com>
2024-09-01 16:28:52 +0800

    Update socket_getIP.py

--- filter/socket_getIP.py
+++ filter/socket_getIP.py
@@ -1,4 +1,7 @@
 import socket
+import vthread
+
+
 def domain_to_ip(domain):
     try:
         ip = socket.gethostbyname(domain.strip())
@@ -6,3 +9,13 @@ def domain_to_ip(domain):
     except socket.error as e:
         print(f"Error converting {domain} to IP: {e}")
         return None
+
+@vthread.pool(1000)
+def thread_domain_to_ip(domain):
+    with open("ip_addresses.txt","a+") as output_file:
+        try:
+            ip = socket.gethostbyname(domain.strip())
+            output_file.write(ip + "\n")
+        except socket.error as e:
+            print(f"Error converting {domain} to IP: {e}")
+            return None
